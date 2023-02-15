eb12c518f8dcd63147608cd93ed2a92e0e1409a8
Mohammed irfad <muhammedirfad43@gmail.com>
2023-02-15 14:20:11 +0530

    simple context provider

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import logo from './logo.svg';
 import StateTutorials from './components/useState/StateTutorials';
 import UseReducer from './components/useReducer/UseReducer'
 import UseReducer2 from './components/useReducer/UseReducer2'
+import UseContext from './components/useContext/UseContext'
 import './App.css';
+import React from 'react';
 
+
+export const userContent = React.createContext()
 function App() {
   return (
     <div className="App">
- 
+ <userContent.Provider value={'mohammed irfad'}>
+  <UseContext/>
+ </userContent.Provider>
    {/* <StateTutorials/> */}
    {/* <UseReducer1/>  use with simple and local state , action */}
    {/* <UseReducer2/>  use with complex state and action (action in terms of object,state) */}
+
     </div>
   );
 }
--- src/components/useContext/UseContext.js
+++ src/components/useContext/UseContext.js
@@ -1,1 +1,31 @@
-rf
+import React,{useState} from 'react';
+// import '../components/useState/StateTutorials.css';
+import { userContent } from '../../App';
+
+const UseContext = () => {
+
+  
+
+  return (
+    <div>
+    
+
+
+<userContent.Consumer>
+ { user =>{
+return (<> hi  from {user}</>)
+  }}
+</userContent.Consumer>
+
+
+    
+    
+
+
+    </div>
+      
+    
+  )
+}
+
+export default UseContext
